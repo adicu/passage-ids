e6201336a0a4f839134e82c7cf2355cee3cf6637
Howon Byun <howonbyun@gmail.com>
2015-03-06 16:19:12 -0500

    miscellaneous

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,11 @@ sudo apt-get install -y python
 sudo apt-get install -y python-pip
 sudo apt-get install -y vim
 sudo pip install flask
-<<<<<<< HEAD
-=======
+
 sudo pip install -r /vagrant/config/requirements.txt
 sudo pip install requests
 sudo pip install flask-sqlalchemy
 sudo pip install flask-whooshalchemy
->>>>>>> upstream/master
+
 
 echo hello
