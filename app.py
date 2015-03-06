ac2191beb34e6476ae80ff7f64268721666d7d02
Howon Byun <howonbyun@gmail.com>
2015-03-06 16:16:58 -0500

    login page route handling

--- app.py
+++ app.py
@@ -3,17 +3,25 @@ __author__ = 'ADI Labs'
 from flask import Flask, render_template, request
 import requests
 from schema import Passage, Title
+# from ContactForm import QuoteForm
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-# quote = Passage(content="Lorem ipsum", title="Iliad", author="Ken")
-# print(quote.content)
+quote = Passage(content="Lorem ipsum", title="Iliad", author="Ken")
+print(quote.content)
 
 @app.route("/")
 def home():
-	return render_template("index.html")
+	# if request.form['button'] == 'login':
+	# 	print("login clicked")
+	# 	login()
+	return render_template("content.html")
 
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+	if request.method == "POST":	
+		return render_template("login.html")
 # def display_passage():
 # 	if request.method == 'POST':
 #         if request.form['submit'] == 'Do Something':
@@ -23,6 +31,10 @@ def home():
 #         else:
 #             pass # unknown
 #     # elif request.method == 'GET':
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Sorry, this page was not found.", 404
 
 if __name__ == '__main__':
 	app.run(host = "0.0.0.0")
