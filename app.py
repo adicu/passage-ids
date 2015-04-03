11edd8f2fc1f8d154b3fc777704de4a432101f6e
Howon Byun <howonbyun@gmail.com>
2015-04-03 15:11:01 -0400

    front end work

--- app.py
+++ app.py
@@ -12,12 +12,10 @@ app.config["DEBUG"] = True
 
 content = generatePassages()
 
-
 @app.route("/")
 def home():
 	randQuote = content[random.randint(0, len(content) - 1)]
 	return render_template("content.html", content2 = randQuote)
-
 
 @app.route("/form", methods = ['GET', 'POST'])
 def form():
@@ -28,10 +26,10 @@ def form():
 	elif request.method == 'GET':
 		return render_template('form.html', form = form)
 
-@app.route("/login", methods = ['GET', 'POST'])
-def login():
-	if request.method == "POST":	
-		return render_template("login.html")
+# @app.route("/login", methods = ['GET', 'POST'])
+# def login():
+# 	if request.method == "POST":	
+# 		return render_template("login.html")
 
 @app.route("/", methods = ['GET', 'POST'])
 def bringCC():
@@ -42,6 +40,59 @@ def bringCC():
 @app.errorhandler(404)
 def page_not_found(error):
     return "Sorry, this page was not found.", 404
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    """
+    Returns an auth code after user logs in through Google+.
+    :param string code: code that is passed in through Google+.
+        Do not provide this yourself.
+    :return: An html page with an auth code.
+    :rtype: flask.Response
+    """
+    # Get code from params.
+    code = request.args.get('code')
+    if not code:
+        return render_template('auth.html',
+                               success=False)
+
+    try:
+        # Exchange code for email address.
+        # Get Google+ ID.
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+        gplus_id = credentials.id_token['sub']
+
+        # Get first email address from Google+ ID.
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+
+        h, content = http.request('https://www.googleapis.com/plus/v1/people/' + gplus_id, 'GET')
+        data = json.loads(content)
+        email = data["emails"][0]["value"]
+
+        # Verify email is valid.
+        regex = re.match(CU_EMAIL_REGEX, email)
+
+        if not regex:
+            return render_template('auth.html',
+                                   success=False,
+                                   reason="You need to log in with your "
+                                   + "Columbia or Barnard email! You logged "
+                                   + "in with: "
+                                   + email)
+
+        # Get UNI and ask database for code.
+        uni = regex.group('uni')
+        code = db.get_oauth_code_for_uni(g.cursor, uni)
+        return render_template('auth.html', success=True, uni=uni, code=code)
+    except Exception as e:
+        # TODO: log errors3
+        print e
+        return render_template('auth.html',
+                               success=False,
+                               reason="An error occurred. Please try again.")
 
 if __name__ == '__main__':
 	app.run(host = '0.0.0.0')
