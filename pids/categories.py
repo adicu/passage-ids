aa94e59ab401a4fdc2a625ad53ab62f62217eec0
Jillian Knoll <jillian.knoll@columbia.edu>
2015-04-24 14:24:02 -0400

    categories updated

--- pids/categories.py
+++ pids/categories.py
@@ -1,8 +1,12 @@
-lithum = {"Greek Tragedy": ["Medea", "Oresteia", "Oedipus the King"],
+lithum1 = {"Greek Tragedy": ["Medea", "Oresteia", "Oedipus the King"],
 		  "Play": ["Medea", "Oresteia", "Oedipus the King", "King Lear"],
 		  "Epic": ["Aeneid", "Odyssey", "Iliad"],
 		  "History" : ["The Histories", "History of the Peloponnesian War"],
 		  "Bible" : ["Luke", "John", "Job", "Exodus", "Genesis"], 
-		  "Roman" : ["The Golden Ass", "Metamorphoses", "Aeneid", "Confessions"], 
-		  "Novel" : ["Don Quixote", "Pride and Prejudice", "To the Lighthouse", "The Golden Ass"]
 		}
+
+lithum2 {"Ancient": ["The Aneid", "Metamorphoses"]
+		"Existential": ["Faust", "Crime and Punishment"]
+		"Medieval": ["Confessions", "Don Quixote", "King Lear"]
+		"Novel": ["Essays", "Pride and Prejudice", "To the Lighthouse" ]
+		}s
